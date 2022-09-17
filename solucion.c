9e5490f236e9f48147dfa5ac9d2ee1e23f41d2b2
Agustin Ranieri <aguseranieri@gmail.com>
2022-09-17 12:02:36 -0300

    Update solucion.c

--- solucion.c
+++ solucion.c
@@ -1,38 +1,6 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <locale.h>
-
-typedef struct 
-{ 
-    int x; 
-    int y; 
-} 
-t_posicion;
-
-typedef struct 
-{ 
-    int** items; 
-    int tamanio;
-    int jugadas;
-} 
-t_tablero;
-
-t_posicion movimientos[] = 
-{
-    {1, -2}, {2, -1}, {2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}
-};
+#include "solucion.h"
 
 unsigned long long int intentos = 0;
-
-bool pertenece(int tamanio, t_posicion posicion);
-bool recorrer(t_tablero tablero, t_posicion caballo);
-bool recorrer_recursivo(t_tablero tablero, t_posicion caballo, int iteracion);
-bool es_ultima_iteracion(t_tablero tablero, int iteracion);
-t_posicion mover(t_posicion posicion, int i);
-bool esta_libre(t_tablero tablero, t_posicion posicion);
-int mostrar(t_tablero tablero);
 
 bool recorrer(t_tablero tablero, t_posicion pos_inicial) 
 {   
